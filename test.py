c5c9068dfdd246d53be4edbdd45dd63925810319
jsayles <jsayles@gmail.com>
2020-09-18 13:48:41 -0700

    fixed method name

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ from LPD8806 import LPD8806
 
 strip = LPD8806(32, 10, 11)
 
-strip.fill(255, 0, 0, pulse=0.3)
-strip.fill(0, 255, 0, pulse=0.3)
-strip.fill(0, 0, 255, pulse=0.3)
+pulse_seconds = 0.3
+strip.fillStrip(255, 0, 0, pulse=pulse_seconds)
+strip.fillStrip(0, 255, 0, pulse=pulse_seconds)
+strip.fillStrip(0, 0, 255, pulse=pulse_seconds)
 strip.off()
