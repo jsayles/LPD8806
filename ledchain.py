e62ff3523a8672d76a90894999a3675d1a908fda
jsayles <jsayles@gmail.com>
2020-09-26 11:52:58 -0700

    new class to abstract the LED Chain

---
+++ ledchain.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+
+
+class LEDChain:
+
+    def __init__(self, bcm_pin):
+        self.pin = bcm_pin
+
+        # Setup the GPIO pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT, initial=GPIO.LOW)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.LOW)
